89be1dee71ac51ccd7f72c2a08bb988974e2ae97
Vasya Dmitrov <vd-boy400@yandex.ru>
2023-09-05 14:33:28 +0300

    update path

--- movies-explorer/src/components/Navigation/Navigation.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './Navigation.css';
-
-const Navigation = ({ children }) => {
-    return <nav className={`navigation`}>{children}</nav>;
-};
-
-export default Navigation;
